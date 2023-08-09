1eea5487672fdd4a5c18ac0cdd5f6c9736593f08
Hugo453 <139181547+Hugo453@users.noreply.github.com>
2023-08-08 20:22:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.attach('camara');
+camera=document.getElementById("camara");
+
+Webcam.set (
+{
+ width:300,
+ heigth:300,
+ image_format:'png',
+ png_quality:90
+});
+
+function capturar_imagen(){
+Webcam.snap (function(data_uri)
+{
+    document.getElementById("resultado").innerHTML='<img id="foto_tomada" src="'+data_uri+'"/>';
+
+}
+);
+ }
